c81ed6c3e28b9d91b0193af71d7808c1aa7a67ca
kdw <doowone8650@gmail.com>
2022-12-12 21:17:06 +0900

    ?

--- SwiftUI_test/ContentView.swift
+++ SwiftUI_test/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 HStack{
+                    
                     MyVstackView()
                     MyVstackView()
                     MyVstackView()
